3467acf1046d12671a79be94a1bac5a2ae58d7e1
vdivekar2013 <vdivekar2013@gmail.com>
2016-10-04 16:39:50 +0530

    Clean up and moved market data reading to Market object

--- Market.js
+++ Market.js
@@ -1,30 +1,38 @@
 define('Market',['InstrumentStore','jquery'],function(InstrumentStore,$) {
+	this.logResults = function(json) {
+		console.log('In callback ' + JSON.stringify(json));
+	};
 	return {
 		'call' : function() {
 			var array = InstrumentStore.getArray();
-			for(var i = 0; i < array.length; i++) {
-				var url = "https://www.google.com/finance/info?q=" + array[i].id;
-				$.ajax({ url: url, 
-						 dataType: "jsonp",
-						 async: false,
-						 jsonpCallback: function(json) {
-							 if(json == undefined) {
-								 alert('data is undefined');
-							 }
-							 alert(JSON.stringify(json));
-							 if(json instanceof Array) {
-								 array[i].value = json[0].l_fix;
-							 }
-						 },
-						 error: function(xhr, ajaxOptions, thrownError) {
-							 alert('i am here 4');
-							 console.log(thrownError);
-						 },
-						 complete: function() {
-							 alert('completed');
-						 }
-					});
-			}
-			InstrumentStore.loadArray(array);
-		}};
+			var url = "https://www.google.com/finance/info?q=";
+			for ( var i = 0; i < array.length; i++)
+				url = url.concat(array[i].id + ',');
+			$.ajax({
+				url : url,
+				dataType : "jsonp",
+				async : false,
+				jsonpCallback : "logResults",
+				// Work with the response
+			    success: function( data ) {
+			    	console.log('In Success ' + JSON.stringify(data)); // server response
+					var array = InstrumentStore.getArray();
+					if (data != undefined && data instanceof Array) {
+						for ( var i = 0; i < data.length; i++) {
+							var id = data[i].e.concat(':').concat(data[i].t);
+							console.log('id = ' + id);
+							for ( var j = 0; j < array.length; j++) {
+								if (array[j].id.toUpperCase() == id.toUpperCase()) {
+									array[j].value = data[i].l_fix;
+									console.log('value = ' + array[j].value);
+									break;
+								}
+							}
+						}
+						InstrumentStore.loadArray(array);
+					}
+			    }
+			});
+		}
+	};
 });
