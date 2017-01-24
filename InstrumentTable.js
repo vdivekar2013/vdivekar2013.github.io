a864475cf629f93c563f8a80463c24bfa26d2492
vdivekar2013 <vdivekar2013@gmail.com>
2017-01-24 15:39:00 +0530

    Add files via upload
    Number formatting

--- InstrumentTable.js
+++ InstrumentTable.js
@@ -134,7 +134,7 @@ define('InstrumentTable',['FOInstrumentStore','FOInstrument','jquery'], function
 						currentPrice = down20Percent + twoPercent * h3;
 						if(headerInstrument != '')
 							rowArray.push(totalPL);
-						rowArray.push(numberFormatter(currentPrice,0));
+						rowArray.push(Math.round(currentPrice));
 						totalPL = 0;
 						headerInstrument = foArray[h2].name;
 					}
