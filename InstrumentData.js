a498a21421bb21996d67a2467bd0970b0788b4a1
vdivekar2013 <vdivekar2013@gmail.com>
2017-10-20 15:33:42 +0530

    Add files via upload
    Cache implemented for instrument data

--- InstrumentData.js
+++ InstrumentData.js
@@ -1,11 +1,23 @@
 define('InstrumentData',['Firebase'],function(firebase) {
 	var lastUpdate;
+	var cache = {};
 	return {
 		'getInstrument' : function(instrument,callback) {
 			if(instrument == '' || instrument == undefined)
 				return;
+			if(lastUpdate == undefined)
+				lastUpdate = new Date();
+			var now = new Date();
+			if((now.getTime() - lastUpdate.getTime()) > 3600000) {
+				cache = {};
+				lastUpdate = new Date();
+			}
+			if(cache[instrument] != undefined) {
+				callback(cache[instrument]);
+				console.log(instrument + ' data, got it from cache');
+			}
 			firebase.database().ref('/instruments/value/' + instrument + '/').once('value').then(function(snapshot) {
-				//console.log('Objects loaded are ' + JSON.stringify(snapshot.val()));
+				cache[instrument] = snapshot.val();
 				callback(snapshot.val());
 			});
 		}
